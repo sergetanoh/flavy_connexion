5df07727c4b2a7b809638f66148210903ddda42e
Lamagnigui TOURE <lamagniguitoure@gmail.com>
2024-11-05 01:17:06 +0000

    Auth

--- api/permissions.py
+++ api/permissions.py
@@ -27,6 +27,10 @@ class IsClientOrReadOnly(permissions.BasePermission):
     """
     def has_permission(self, request, view):
         if request.method == 'POST':
+
+            # Vérifiez si l'utilisateur est authentifié avant de vérifier `is_pharmacie`
+            if not request.user.is_authenticated:
+                return False
             # Seuls les clients peuvent passer des commandes
             return request.user.is_pharmacie == False
         elif request.method == 'GET':
@@ -43,6 +47,10 @@ class IsPharmacieCanModifyCommande(permissions.BasePermission):
     Permission personnalisée pour autoriser uniquement les pharmacies à modifier ou supprimer des commandes.
     """
     def has_object_permission(self, request, view, obj):
+
+         # Vérifiez si l'utilisateur est authentifié avant de vérifier `is_pharmacie`
+        if not request.user.is_authenticated:
+            return False
         # Seule la pharmacie qui a posté le produit peut modifier ou supprimer la commande
         return request.user and request.user.is_authenticated and request.user.is_pharmacie == True
 
@@ -52,6 +60,10 @@ class IsPharmacieOrClient(permissions.BasePermission):
     Permission personnalisée pour autoriser uniquement les pharmacies à modifier ou supprimer des commandes.
     """
     def has_object_permission(self, request, view, obj):
+
+        # Vérifiez si l'utilisateur est authentifié avant de vérifier `is_pharmacie`
+        if not request.user.is_authenticated:
+            return False
         # Seul les clients ou les pharmacies peuvent effectuer des actions
         return request.user and request.user.is_authenticated
 
